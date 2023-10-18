5be6880fb0572dbb3772d0c7aaf597c738fbebf8
Jacky <jackypengsmail@gmail.com>
2023-10-18 16:03:38 -0400

    Implement total average function

--- SBA308/index.js
+++ SBA308/index.js
@@ -76,29 +76,56 @@ const LearnerSubmissions = [
   },
 ];
 
-// function getAverage(learnerId) {
+function getTotalAverage(learnerId) {
+  let totalScore = 0;
+  let totalMaxScore = 0;
+  let assignmentIdsOfLearner = [];
+  LearnerSubmissions.forEach((submission) => {
+    console.log(
+      submission.learner_id === learnerId,
+      submission.learner_id,
+      learnerId
+    );
+    if (submission.learner_id === learnerId) {
+      console.log("asdasd");
+      assignmentIdsOfLearner.push(submission.assignment_id);
+      totalScore += submission.submission.score;
+    }
+  });
 
-// }
+  AssignmentGroup.assignments.forEach((assignment) => {
+    if (assignmentIdsOfLearner.includes(assignment.id)) {
+      totalMaxScore += assignment.points_possible;
+    }
+  });
 
+  console.log(totalScore / totalMaxScore);
+  console.log(totalScore, totalMaxScore, assignmentIdsOfLearner);
+  return totalScore / totalMaxScore;
+}
+
+getTotalAverage(132);
 function getResult(scoresArray) {
   let results = [];
   for (let item of scoresArray) {
-      // console.log(item, "item");
+    // console.log(item, "item");
     //   console.log(item.learner_id)
     //   const existingItemIndex = results.indexOf(item.learner_id);
-      const existingItemIndex = results.findIndex(result => result.learner_id === item.learner_id)
-      console.log(existingItemIndex)
+    const existingItemIndex = results.findIndex(
+      (result) => result.learner_id === item.learner_id
+    );
+    console.log(existingItemIndex);
     //   console.log(existingItemIndex === -1)
-      if (existingItemIndex !== -1) {
-        //   console.log("hello")
-        //   console.log(existingItemIndex);
-          //   console.log(results[existingItemIndex]);
-          console.log({ ...results[existingItemIndex], ...item })
-          results[existingItemIndex] = { ...results[existingItemIndex], ...item }
-        //   results[existingItemIndex].item.assignment_id = item.score;
-      } else {
-        results.push(item)
-    //   results.push({ id: item.learner_id, keyy: item.score });
+    if (existingItemIndex !== -1) {
+      //   console.log("hello")
+      //   console.log(existingItemIndex);
+      //   console.log(results[existingItemIndex]);
+      console.log({ ...results[existingItemIndex], ...item });
+      results[existingItemIndex] = { ...results[existingItemIndex], ...item };
+      //   results[existingItemIndex].item.assignment_id = item.score;
+    } else {
+      results.push(item);
+      //   results.push({ id: item.learner_id, keyy: item.score });
     }
     // const newArray = newArray.map(item => {
     //     if (item.)
@@ -110,10 +137,9 @@ function getResult(scoresArray) {
     //         results.push({ id: item.learner_id, item.assignment_id: item.score})
     //     }
 
-    console.log(results);
+    // console.log(results);
   }
 }
-
 
 function getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions) {
   // console.log(CourseInfo, AssignmentGroup, "ASSIGNMENTS", LearnerSubmissions, "learnersubmissions")
@@ -124,55 +150,47 @@ function getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions) {
     const relatedAssignment = AssignmentGroup.assignments.find(
       (assignment) => assignment.id === learner.assignment_id
     );
-      // console.log(relatedAssignment, "relatedassign");
-      const assignment_id = learner.assignment_id
+    // console.log(relatedAssignment, "relatedassign");
+    const assignment_id = learner.assignment_id;
     //   console.log(assignment_id)
-      let person = {}
-      person["learner_id"] = learner.learner_id
-      person[assignment_id] = learner.submission.score / relatedAssignment.points_possible
-      return person
-      return {
-        
+    let person = {};
+    person["learner_id"] = learner.learner_id;
+    person[assignment_id] =
+      learner.submission.score / relatedAssignment.points_possible;
+    return person;
+    return {
       learner_id: learner.learner_id,
-    //   assignment_id: learner.assignment_id,
-      assignment_id: learner.submission.score / relatedAssignment.points_possible,
+      //   assignment_id: learner.assignment_id,
+      assignment_id:
+        learner.submission.score / relatedAssignment.points_possible,
     };
   });
-    //   console.log(scores);
-    [
-        { learner_id: 125, assignment_id: 1, score: 0.94 },
-        { learner_id: 125, assignment_id: 2, score: 1 },
-        { learner_id: 125, assignment_id: 3, score: 0.8 },
-        { learner_id: 132, assignment_id: 1, score: 0.78 },
-        { learner_id: 132, assignment_id: 2, score: 0.9333333333333333 }
-    ]
-    
-    // to this 
-    const result = [
-        {
-          id: 125,
-          avg: 0.985, // (47 + 150) / (50 + 150)
-          1: 0.94, // 47 / 50
-          2: 1.0 // 150 / 150
-        },
-        {
-          id: 132,
-          avg: 0.82, // (39 + 125) / (50 + 150)
-          1: 0.78, // 39 / 50
-          2: 0.833 // late: (140 - 15) / 150
-        }
-    ];
+  //   console.log(scores);
+  [
+    { learner_id: 125, assignment_id: 1, score: 0.94 },
+    { learner_id: 125, assignment_id: 2, score: 1 },
+    { learner_id: 125, assignment_id: 3, score: 0.8 },
+    { learner_id: 132, assignment_id: 1, score: 0.78 },
+    { learner_id: 132, assignment_id: 2, score: 0.9333333333333333 },
+  ];
 
+  // to this
+  const result = [
+    {
+      id: 125,
+      avg: 0.985, // (47 + 150) / (50 + 150)
+      1: 0.94, // 47 / 50
+      2: 1.0, // 150 / 150
+    },
+    {
+      id: 132,
+      avg: 0.82, // (39 + 125) / (50 + 150)
+      1: 0.78, // 39 / 50
+      2: 0.833, // late: (140 - 15) / 150
+    },
+  ];
 
-    
-
-
-    
-
-
-
-
-  console.log(getResult(scores));
+  //   console.log(getResult(scores));
 }
 
 getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);

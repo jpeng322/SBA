660c2f5aabebd2181fdcb07f9a14045c6e49dca0
Jacky <jackypengsmail@gmail.com>
2023-10-18 15:09:42 -0400

    Update scores method to restructure return obj

--- SBA308/index.js
+++ SBA308/index.js
@@ -77,14 +77,36 @@ const LearnerSubmissions = [
 ];
 
 // function getAverage(learnerId) {
-    
+
 // }
 
-// function getResult(scoresArray) {
-//     for (let item of scoresArray) {
-//         console.log(item)
-//     }
-// }
+function getResult(scoresArray) {
+  let results = [];
+  for (let item of scoresArray) {
+    // console.log(item, "item");
+    const existingItemIndex = results.indexOf(item.learner_id);
+      if (existingItemIndex !== -1) {
+          console.log(existingItemIndex);
+          console.log(results[existingItemIndex]);
+          results[existingItemIndex].item.assignment_id = item.score;
+      } else {
+        results.push(item)
+    //   results.push({ id: item.learner_id, keyy: item.score });
+    }
+    // const newArray = newArray.map(item => {
+    //     if (item.)
+    // } )
+    //     for (let result of results) {
+
+    //       if (result.learner_id)
+    // } else {
+    //         results.push({ id: item.learner_id, item.assignment_id: item.score})
+    //     }
+
+    console.log(results);
+  }
+}
+
 
 function getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions) {
   // console.log(CourseInfo, AssignmentGroup, "ASSIGNMENTS", LearnerSubmissions, "learnersubmissions")
@@ -95,28 +117,55 @@ function getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions) {
     const relatedAssignment = AssignmentGroup.assignments.find(
       (assignment) => assignment.id === learner.assignment_id
     );
-    // console.log(relatedAssignment, "relatedassign");
-    return {
-        learner_id: learner.learner_id,
-        assignment_id: learner.assignment_id,
-      score: learner.submission.score / relatedAssignment.points_possible
+      // console.log(relatedAssignment, "relatedassign");
+      const assignment_id = learner.assignment_id
+    //   console.log(assignment_id)
+      let person = {}
+      person["learner_id"] = learner.learner_id
+      person[assignment_id] = learner.submission.score / relatedAssignment.points_possible
+      return person
+      return {
+        
+      learner_id: learner.learner_id,
+    //   assignment_id: learner.assignment_id,
+      assignment_id: learner.submission.score / relatedAssignment.points_possible,
     };
   });
-  console.log(scores);
-  // const result = [
-  //     {
-  //       id: 125,
-  //       avg: 0.985, // (47 + 150) / (50 + 150)
-  //       1: 0.94, // 47 / 50
-  //       2: 1.0 // 150 / 150
-  //     },
-  //     {
-  //       id: 132,
-  //       avg: 0.82, // (39 + 125) / (50 + 150)
-  //       1: 0.78, // 39 / 50
-  //       2: 0.833 // late: (140 - 15) / 150
-  //     }
-  //   ];
+    //   console.log(scores);
+    [
+        { learner_id: 125, assignment_id: 1, score: 0.94 },
+        { learner_id: 125, assignment_id: 2, score: 1 },
+        { learner_id: 125, assignment_id: 3, score: 0.8 },
+        { learner_id: 132, assignment_id: 1, score: 0.78 },
+        { learner_id: 132, assignment_id: 2, score: 0.9333333333333333 }
+    ]
+    
+    // to this 
+    const result = [
+        {
+          id: 125,
+          avg: 0.985, // (47 + 150) / (50 + 150)
+          1: 0.94, // 47 / 50
+          2: 1.0 // 150 / 150
+        },
+        {
+          id: 132,
+          avg: 0.82, // (39 + 125) / (50 + 150)
+          1: 0.78, // 39 / 50
+          2: 0.833 // late: (140 - 15) / 150
+        }
+    ];
+
+
+    
+
+
+    
+
+
+
+
+  console.log(getResult(scores));
 }
 
 getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);

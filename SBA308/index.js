97216faaf1206a2feb08c872afd968f2ff5aa46d
Jacky <jackypengsmail@gmail.com>
2023-10-18 14:02:42 -0400

    Get average of each assignment

---
+++ SBA308/index.js
@@ -0,0 +1,122 @@
+// The provided course information.
+const CourseInfo = {
+  id: 451,
+  name: "Introduction to JavaScript",
+};
+
+// The provided assignment group.
+const AssignmentGroup = {
+  id: 12345,
+  name: "Fundamentals of JavaScript",
+  course_id: 451,
+  group_weight: 25,
+  assignments: [
+    {
+      id: 1,
+      name: "Declare a Variable",
+      due_at: "2023-01-25",
+      points_possible: 50,
+    },
+    {
+      id: 2,
+      name: "Write a Function",
+      due_at: "2023-02-27",
+      points_possible: 150,
+    },
+    {
+      id: 3,
+      name: "Code the World",
+      due_at: "3156-11-15",
+      points_possible: 500,
+    },
+  ],
+};
+
+// The provided learner submission data.
+const LearnerSubmissions = [
+  {
+    learner_id: 125,
+    assignment_id: 1,
+    submission: {
+      submitted_at: "2023-01-25",
+      score: 47,
+    },
+  },
+  {
+    learner_id: 125,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-02-12",
+      score: 150,
+    },
+  },
+  {
+    learner_id: 125,
+    assignment_id: 3,
+    submission: {
+      submitted_at: "2023-01-25",
+      score: 400,
+    },
+  },
+  {
+    learner_id: 132,
+    assignment_id: 1,
+    submission: {
+      submitted_at: "2023-01-24",
+      score: 39,
+    },
+  },
+  {
+    learner_id: 132,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-03-07",
+      score: 140,
+    },
+  },
+];
+
+// function getAverage(learnerId) {
+    
+// }
+
+// function getResult(scoresArray) {
+//     for (let item of scoresArray) {
+//         console.log(item)
+//     }
+// }
+
+function getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions) {
+  // console.log(CourseInfo, AssignmentGroup, "ASSIGNMENTS", LearnerSubmissions, "learnersubmissions")
+  // console.log(AssignmentGroup)
+  // const courseAssignment = AssignmentGroup.find(assignment => assignment.course_id === CourseInfo.id)
+  // console.log(courseAssignment)
+  const scores = LearnerSubmissions.map((learner) => {
+    const relatedAssignment = AssignmentGroup.assignments.find(
+      (assignment) => assignment.id === learner.assignment_id
+    );
+    // console.log(relatedAssignment, "relatedassign");
+    return {
+        learner_id: learner.learner_id,
+        assignment_id: learner.assignment_id,
+      score: learner.submission.score / relatedAssignment.points_possible
+    };
+  });
+  console.log(scores);
+  // const result = [
+  //     {
+  //       id: 125,
+  //       avg: 0.985, // (47 + 150) / (50 + 150)
+  //       1: 0.94, // 47 / 50
+  //       2: 1.0 // 150 / 150
+  //     },
+  //     {
+  //       id: 132,
+  //       avg: 0.82, // (39 + 125) / (50 + 150)
+  //       1: 0.78, // 39 / 50
+  //       2: 0.833 // late: (140 - 15) / 150
+  //     }
+  //   ];
+}
+
+getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);

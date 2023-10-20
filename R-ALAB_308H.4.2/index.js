f0ec4933e5c17a471d1619a369702e89d5173baf
Jacky <jackypengsmail@gmail.com>
2023-10-20 13:13:46 -0400

    Yell at the Ninja Turtles answered

--- R-ALAB_308H.4.2/index.js
+++ R-ALAB_308H.4.2/index.js
@@ -11,7 +11,7 @@
 // }
 
 
-//FizzBuzz 
+//FizzBuzz
 // for (let i = 1; i <= 100; i++) {
 //     if (i % 3 === 0 && i % 5 === 0) {
 //         console.log("FizzBuzz", i)
@@ -19,20 +19,29 @@
 //         console.log("Buzz", i)
 //     } else if (i % 3 === 0) {
 //         console.log("Fizz", i)
-//     } 
+//     }
 // }
 
 //Wild Wild Life
 
-const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
-const sharky = ["Sharky", "shark", 20, "Left Coast"]
-const plantee = ["Plantee", "plant",  5000 , "Mordor"]
-const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
-const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"]
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+// const sharky = ["Sharky", "shark", 20, "Left Coast"]
+// const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+// const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"]
 
-plantee[2] = 5001
-wolfy[3] = "Gotham City"
-dart.push("Hawkins")
-wolfy[0] = "Gameboy"
+// plantee[2] = 5001
+// wolfy[3] = "Gotham City"
+// dart.push("Hawkins")
+// wolfy[0] = "Gameboy"
 
-console.log(wolfy, plantee, dart)
+// console.log(wolfy, plantee, dart)
+
+// Yell at the Ninja Turtles
+
+const turtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"]
+
+for (let name of turtles) {
+    console.log(name.toUpperCase())
+}
+

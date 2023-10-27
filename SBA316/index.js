91bfb0db60dc913735c86abaeac180e33b15a16e
Jacky <jackypengsmail@gmail.com>
2023-10-27 10:02:19 -0400

    Added textbox to body

--- SBA316/index.js
+++ SBA316/index.js
@@ -3,3 +3,10 @@ console.log("connected");
 
 console.log(data);
 
+const textBox = document.createElement("div")
+
+const body = document.querySelector("body")
+body.append(textBox)
+
+textBox.textContent = "sadasd"
+
